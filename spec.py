f3bd314bdd06659674f61f0ec6b6c8bb073a5bc4
Zimri Leisher <zimri.leisher@gmail.com>
2026-08-11 11:19:42 -0400

    Iterate on spec

--- spec.py
+++ spec.py
@@ -1,5 +1,5 @@
-from dataclasses import dataclass
 from enum import Enum
+from pathlib import Path
 from typing import (
     Generic,
     Mapping,
@@ -22,8 +22,11 @@ class CompileError(Exception):
 
 # syntax
 
+type Id = int
+
 
 class Ident(NamedTuple):
+    id: Id
     text: str
 
 
@@ -31,99 +34,118 @@ type QualifiedIdent = ProperQualifiedIdent | Ident
 
 
 class ProperQualifiedIdent(NamedTuple):
+    id: Id
     qualifier: QualifiedIdent
     ident: Ident
 
 
 class VarDef(NamedTuple):
+    id: Id
     ident: Ident
 
 
 class FuncDef(NamedTuple):
+    id: Id
     ident: Ident
 
 
 class ModuleDef(NamedTuple):
+    id: Id
     ident: Ident
 
 
+class SequenceDef(NamedTuple):
+    id: Id
+    path: Path
+
+
+class DirectoryDef(NamedTuple):
+    id: Id
+    path: Path
+
+
+# from dict
 class TypeDef(NamedTuple):
+    id: Id
     ident: Ident
 
 
+# from dict
 class EnumConstantDef(NamedTuple):
+    id: Id
     ident: Ident
 
 
+# from dict
 class TypeCtorDef(NamedTuple):
+    id: Id
     ident: Ident
 
 
-type Definition = VarDef | FuncDef | ModuleDef | TypeDef | EnumConstantDef | TypeCtorDef
+# from dict
+class CommandDef(NamedTuple):
+    id: Id
+    ident: Ident
 
 
 # semantics
 
-# Symbols are entities: one symbol exists per definition (per module, for
-# ModuleSymbol), and equality is object identity. Syntax compares
-# structurally; symbols do not.
+
+class VarSymbol(NamedTuple):
+    defn: VarDef
 
 
-@dataclass(eq=False)
-class VarSymbol:
-    definition: VarDef
+class FuncSymbol(NamedTuple):
+    defn: FuncDef
 
 
-@dataclass(eq=False)
-class FuncSymbol:
-    definition: FuncDef
-
-
-@dataclass(eq=False)
-class ModuleSymbol:
+class ModuleSymbol(NamedTuple):
     # a module may be defined by several syntactic defs
-    definitions: Sequence[ModuleDef]
+    defns: Sequence[ModuleDef]
 
 
-@dataclass(eq=False)
-class TypeSymbol:
-    definition: TypeDef
+class TypeSymbol(NamedTuple):
+    defn: TypeDef
 
 
-@dataclass(eq=False)
-class EnumConstantSymbol:
-    definition: EnumConstantDef
+class EnumConstantSymbol(NamedTuple):
+    defn: EnumConstantDef
 
 
-@dataclass(eq=False)
-class TypeCtorSymbol:
-    definition: TypeCtorDef
+class TypeCtorSymbol(NamedTuple):
+    defn: TypeCtorDef
 
 
-type Symbol = VarSymbol | FuncSymbol | ModuleSymbol | TypeSymbol | EnumConstantSymbol | TypeCtorSymbol
+class DirectorySymbol(NamedTuple):
+    defn: DirectoryDef
+
+
+class SequenceSymbol(NamedTuple):
+    defn: SequenceDef
+
+
+class CommandSymbol(NamedTuple):
+    defn: CommandDef
+
 
 type ValueSymbol = VarSymbol | EnumConstantSymbol
 
-type CallableSymbol = FuncSymbol | TypeCtorSymbol
+type CallableSymbol = FuncSymbol | TypeCtorSymbol | CommandSymbol
 
-type QualifierSymbol = ModuleSymbol | TypeSymbol
+type QualifierSymbol = ModuleSymbol | DirectorySymbol | SequenceSymbol
 
-
-# it's a name when it refers to something
-S = TypeVar("S", bound=Symbol, covariant=True)
+type Symbol = ValueSymbol | CallableSymbol | TypeSymbol | QualifierSymbol
 
 
-class Name(NamedTuple, Generic[S]):
-    """A name referring to a symbol of type S"""
-
-    ident: Ident
-    referent: S
+type Name = str
 
 
-# TODO I'd really like to be able to say that:
-# for S in ValueSymbol, the Qualifier refs to a Mod or Type (to support enum )
-# everywhere else, Qualifier refs to just a Mod
-type QualifiedName[S: Symbol] = tuple[QualifiedName[QualifierSymbol], Name[S]] | Name[S]
+class ProperQualifiedName(NamedTuple):
+    qualifier: "QualifiedName"
+    name: Name
+
+
+type QualifiedName = Name | ProperQualifiedName
 
 
 class NameGroup[S: Symbol]:
@@ -137,41 +159,58 @@ TypeNameGroup = NameGroup[TypeSymbol | QualifierSymbol]()
 
 
 class Scope(NamedTuple):
-    # cannot express that the S in ng[S] should be sq[name[S]] (per key) so we
-    # define that in the names_in_group function
-    names: Mapping[NameGroup[Symbol], Sequence[Name[Symbol]]]
+    names: Mapping[NameGroup[Symbol], Mapping[Name, Symbol]]
     parent: "Scope|None"
 
-    def names_in_group[S: Symbol](self, group: NameGroup[S]) -> Sequence[Name[S]]:
-        return cast(Sequence[Name[S]], self.names[group])
+    def names_in_group[S: Symbol](self, group: NameGroup[S]) -> Mapping[Name, S]:
+        return cast(Mapping[Name, S], self.names[group])
+
+
+class CompileState(NamedTuple):
+    parent_symbol: Mapping[Symbol, Symbol]
+    symbol_scope: Mapping[QualifierSymbol, Scope]
+    use_def: Mapping[Id, Symbol]
+    type: Mapping[Id, TypeSymbol]
+    value: Mapping[Id, ValueSymbol]
+
+
+# def get_qualifier_scope(sym: QualifierSymbol) -> Scope:
+#     parent_scope =
 
 
 def resolve_ident[S: Symbol](
-    ident: Ident, scope: Scope, name_group: NameGroup[S]
-) -> Name[S]:
+    ident: Ident, scope: Scope, name_group: NameGroup[S], state: CompileState
+) -> S:
     names_in_group_in_scope = scope.names_in_group(name_group)
-    for name in names_in_group_in_scope:
-        if name.ident == ident:
-            return name
+    for name, sym in names_in_group_in_scope.items():
+        if name == ident.text:
+            return sym
 
     if scope.parent is None:
         raise CompileError(Error.QUALIFIED_IDENTIFIER_RESOLVE_FAILED)
 
-    return resolve_ident(ident, scope.parent, name_group)
+    return resolve_ident(ident, scope.parent, name_group, state)
 
 
 def resolve_proper_qual_ident[S: Symbol](
-    qi: ProperQualifiedIdent, scope: Scope, name_group: NameGroup[S]
-) -> QualifiedName[S]:
-    qualifier_name = resolve_qual_ident(qi.qualifier, scope, name_group)
+    qi: ProperQualifiedIdent,
+    scope: Scope,
+    name_group: NameGroup[S],
+    state: CompileState,
+) -> S:
+    qualifier_sym = resolve_qual_ident(qi.qualifier, scope, name_group)
 
-    if not isinstance(qualifier_name, QualifierSymbol):
+    if not isinstance(qualifier_sym, QualifierSymbol):
         raise CompileError(Error.QUALIFIER_IDENT_RESOLVED_TO_NON_QUALIFIER_SYMBOL)
+
+    qualifier_scope = state.symbol_scope[qualifier_sym]
+
+    return resolve_ident(qi.ident, qualifier_scope, name_group, state)
 
 
 def resolve_qual_ident[S: Symbol](
-    qi: QualifiedIdent, scope: Scope, name_group: NameGroup[S]
-) -> QualifiedName[S]:
+    qi: QualifiedIdent, scope: Scope, name_group: NameGroup[S], state: CompileState
+) -> S:
     if isinstance(qi, Ident):
-        return resolve_ident(qi, scope, name_group)
-    return resolve_proper_qual_ident(qi, scope, name_group)
+        return resolve_ident(qi, scope, name_group, state)
+    return resolve_proper_qual_ident(qi, scope, name_group, state)
